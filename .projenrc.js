8e59a4feddf7a9e347544b2e6b65f65478ddf820
Marcio Cruz <marciocadev@gmail.com>
2021-12-28 15:58:17 -0300

    chore(init): first commit

--- .projenrc.js
+++ .projenrc.js
@@ -7,10 +7,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: 'teste-projen-cdk',
   repositoryUrl: 'https://github.com/marciocadev/teste-projen-cdk.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-  // release: undefined,      /* Add release management to this project. */
+  antitamper: false,
+  release: true,
+  docgen: true,
+  eslint: true,
+  tsconfig: {
+    compilerOptions: {
+      lib: ['dom', 'es2019'],
+    },
+  },
 });
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { Function, FunctionProps } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+
+export class TesteProjenCdk extends Function {
+  constructor(scope: Construct, id: string, props: FunctionProps) {
+
+    super(scope, id, {
+      code: props.code,
+      runtime: props.runtime,
+      handler: props.handler
+    });
   }
 }
